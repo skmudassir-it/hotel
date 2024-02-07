4d8fb218f495c5902e59ba4700114612168d54de
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2024-02-07 22:55:02 +0000

    setup nodemon

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server rinning on port using nodemon ‎️‍🔥"));
