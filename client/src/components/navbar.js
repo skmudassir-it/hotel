7745688621cc913ff24f937327ee92c82c3efdc8
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2024-02-07 16:19:06 +0000

    nav bar-1

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -11,10 +11,10 @@ function Navbar() {
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Register</a>
+                    <a class="nav-link" href="/register">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="/login">Login</a>
                 </li>
                 </ul>
             </div>
